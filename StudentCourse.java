b2b24291113aba7c2063e8088fe3dd355f305d46
01fe22bca002 <01fe22bca002@kletech.ac.in>
2024-02-18 23:06:31 +0530

    Code for display Student Course

--- StudentCourse.java
+++ StudentCourse.java
@@ -5,7 +5,7 @@ public class StudentCourse {
         String prg = "BCA";
         String dept = "MCA";
         String[] course={"DevOps","Machine Learning","C#"};
-        System.out.println("Name of the student is = "+name);
+        System.out.println("Name of the student iss = "+name);
         System.out.println("Program of the student is = "+prg);
         System.out.println("Department of the student is = "+dept);
         for (int i=0;i<course.length;i++){
